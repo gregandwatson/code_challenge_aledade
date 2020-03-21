ebac4c1beacb9443941c791620074a556966fd5c
gregandwatson <greg@blacksintechnology.net>
2020-03-21 17:29:12 -0400

    added user data and subnet id variable override

--- modules/postgres_vm/main.tf
+++ modules/postgres_vm/main.tf
@@ -1,5 +1,13 @@
 resource "aws_instance" "example" {
   ami           = "${var.image_id}"
   instance_type = "${var.instance_type}"
+  subnet_id = var.vpc_subnet_id
 
+  user_data    = <<-EOF
+                  #!/bin/bash
+                  sudo yum -y update
+                  sudo yum -y install epel-repo
+                  sudo yum -y update
+                  yum -y install ansible
+                  EOF
 }
