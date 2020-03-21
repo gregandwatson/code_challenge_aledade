da608d9b5be1db2710051977612c763641116511
gregandwatson <greg@blacksintechnology.net>
2020-03-21 17:27:07 -0400

    added subnets to locals

--- mod_locals.tf
+++ mod_locals.tf
@@ -1,14 +1,14 @@
 locals {
   # Common tags to be assigned to all resources
   dev = {
-    vpc_cidr_block = local.service_name
-    subnet_cidr_block   = local.owner
+    vpc_cidr_block = "10.1.0.0/16"
+    subnet_cidr_block   = "10.1.1.0/24"
     instance_type = "t2.micro"
     image_id	= "ami-66506c1c"
   }
   prod = {
-    vpc_cidr_block = local.service_name
-    subnet_cidr_block   = local.owner
+    vpc_cidr_block = "10.2.0.0/16"
+    subnet_cidr_block   = "10.2.1.0/24"
     instance_type = "t2.micro"
     image_id	= "ami-66506c1c"
   }
