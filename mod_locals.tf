9d53dffb3548d6f2983a79dc795af20266e7a606
gregandwatson <greg@blacksintechnology.net>
2020-03-22 13:44:03 -0400

    added playbook file name

--- mod_locals.tf
+++ mod_locals.tf
@@ -5,11 +5,13 @@ locals {
     subnet_cidr_block   = "10.1.1.0/24"
     instance_type = "t2.micro"
     image_id	= "ami-66506c1c"
+    playbook = "postgres-dev.yml"
   }
   prod = {
     vpc_cidr_block = "10.2.0.0/16"
     subnet_cidr_block   = "10.2.1.0/24"
     instance_type = "t2.micro"
     image_id	= "ami-66506c1c"
+    playbook = "postgres-prod.yml"
   }
 }
