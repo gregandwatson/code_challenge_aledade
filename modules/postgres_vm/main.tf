c2793ea734bf654691f984c0f2a74d0072c60fad
gregandwatson <greg@blacksintechnology.net>
2020-03-22 09:49:20 -0400

    removing user data from module to make module more generic

--- modules/postgres_vm/main.tf
+++ modules/postgres_vm/main.tf
@@ -2,12 +2,4 @@ resource "aws_instance" "example" {
   ami           = "${var.image_id}"
   instance_type = "${var.instance_type}"
   subnet_id = var.vpc_subnet_id
-
-  user_data    = <<-EOF
-                  #!/bin/bash
-                  sudo yum -y update
-                  sudo yum -y install epel-repo
-                  sudo yum -y update
-                  yum -y install ansible
-                  EOF
 }
