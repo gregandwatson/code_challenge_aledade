e47495c42b58a5e6f3c1da4fc6157b368daab9e1
gregandwatson <greg@blacksintechnology.net>
2020-03-23 20:37:06 -0400

    modified local variables so can be used per environment

--- mod_locals.tf
+++ mod_locals.tf
@@ -1,17 +1,23 @@
 locals {
   # Common tags to be assigned to all resources
-  dev = {
-    vpc_cidr_block = "10.1.0.0/16"
-    subnet_cidr_block   = "10.1.1.0/24"
-    instance_type = "t2.micro"
-    image_id	= "ami-66506c1c"
-    playbook = "postgres-dev.yml"
-  }
-  prod = {
-    vpc_cidr_block = "10.2.0.0/16"
-    subnet_cidr_block   = "10.2.1.0/24"
-    instance_type = "t2.micro"
-    image_id	= "ami-66506c1c"
-    playbook = "postgres-prod.yml"
-  }
+  environment = {
+    dev = {
+      vpc_cidr_block = "10.1.0.0/16"
+      subnet_cidr_block   = "10.1.1.0/24"
+      instance_type = "t2.micro"
+      image_id	= "ami-66506c1c"
+      playbook = "postgres-dev.yml"
+    }
+    prod = {
+      vpc_cidr_block = "10.2.0.0/16"
+      subnet_cidr_block   = "10.2.1.0/24"
+      instance_type = "t2.micro"
+      image_id	= "ami-66506c1c"
+      playbook = "postgres-prod.yml"
+    }
+}
+
+env = merge(
+    local.environment[var.cluster_id]
+    )
 }
