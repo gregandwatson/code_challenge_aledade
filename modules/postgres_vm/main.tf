d8e5dd0b033bd54f6e6bbe2a9d029fa16f8f30bf
gregandwatson <greg@blacksintechnology.net>
2020-03-23 20:38:24 -0400

    moved user data and provisioner to resource

--- modules/postgres_vm/main.tf
+++ modules/postgres_vm/main.tf
@@ -1,5 +1,13 @@
-resource "aws_instance" "example" {
-  ami           = "${var.image_id}"
-  instance_type = "${var.instance_type}"
+resource "aws_instance" "main" {
+  ami           = var.image_id
+  instance_type = var.instance_type
   subnet_id = var.vpc_subnet_id
+  user_data    = <<-EOF
+                  #!/bin/bash
+                  sudo yum -y update
+                  sudo yum -y install python
+                  EOF
+  provisioner "local-exec" {
+    command = "ansible-playbook -u fedora -i '${aws_instance.main.public_ip},' --private-key ${var.ssh_key_private} ${var.postgres_playbook}"
+  }
 }
