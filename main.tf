2ba5c7953d5f683e52704cf0ba3ed686e8d64619
gregandwatson <greg@blacksintechnology.net>
2020-03-28 19:13:46 -0400

    added template file to create host file for ansible

--- main.tf
+++ main.tf
@@ -34,3 +34,15 @@ resource "aws_key_pair" "veronika" {
   key_name   = "veronika-key"
   public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCoTaAVCxUwaMu9Ot3UszKW4jrLalPZ/6EP6JstS9pWwTWduJHvTj0yCIYqPl/xAchgvYAv1693qn+GzQlPRgK4GDwMZe6NMcSmFCKJetxytH2ozeX48sr1YshCmkXtPGGbwYw/B9amBbZZXBkuuFFvDYGDw9n+blQnRXxtNaLU2AvdQ7OGtFweyFdZUwG14eZjc5KHUd+A7paxE6RWqkx6hU2kh9tccF+VY5aLZz5OGTPwVpOqS1Vx6bWZrzOjIkIvzLNr//iOAkJYYYmjwB9zMu6Gc/a5tR7B4VuxCX3YfljDyQ/QcFTYWxQ1hCNbcKy2LDR0/PDXKxTZQ81+fp+z ggreenlee@veronika"
 }
+
+data "template_file" "hosts" {
+  template = file("host.tpl")
+  vars = {
+    dev_ip = module.postgres_instance.postgres_public_ip
+  }
+}
+
+resource "local_file" "hosts" {
+    content     = data.template_file.hosts.rendered
+    filename = "hosts"
+}
