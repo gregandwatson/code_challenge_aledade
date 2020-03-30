054e6ba1d5d09b72459e85899452b08801d0a053
gregandwatson <greg@blacksintechnology.net>
2020-03-29 20:21:35 -0400

    removed unneeded resource, removed public key and replaced with variable to make more generic

--- main.tf
+++ main.tf
@@ -13,27 +13,9 @@ module "vpc" {
   subnet_cidr_block = local.env.subnet_cidr_block
 }
 
-//
-// resource "aws_security_group" "allow_ssh" {
-//   name        = "allow_ssh"
-//   description = "Allow SSH inbound traffic"
-//   vpc_id      = "${aws_vpc.greg-ohio-test.id}"
-//
-//   ingress {
-//     # TLS (change to whatever ports you need)
-//     from_port = 22
-//     to_port   = 22
-//     protocol  = "tcp"
-//
-//     # Please restrict your ingress to only necessary IPs and ports.
-//     # Opening to 0.0.0.0/0 can lead to security vulnerabilities.
-//     cidr_blocks = ["74.83.20.195/32"]
-//   }
-// }
-
 resource "aws_key_pair" "veronika" {
   key_name   = "veronika-key"
-  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCoTaAVCxUwaMu9Ot3UszKW4jrLalPZ/6EP6JstS9pWwTWduJHvTj0yCIYqPl/xAchgvYAv1693qn+GzQlPRgK4GDwMZe6NMcSmFCKJetxytH2ozeX48sr1YshCmkXtPGGbwYw/B9amBbZZXBkuuFFvDYGDw9n+blQnRXxtNaLU2AvdQ7OGtFweyFdZUwG14eZjc5KHUd+A7paxE6RWqkx6hU2kh9tccF+VY5aLZz5OGTPwVpOqS1Vx6bWZrzOjIkIvzLNr//iOAkJYYYmjwB9zMu6Gc/a5tR7B4VuxCX3YfljDyQ/QcFTYWxQ1hCNbcKy2LDR0/PDXKxTZQ81+fp+z ggreenlee@veronika"
+  public_key = var.public_key
 }
 
 data "template_file" "hosts" {
